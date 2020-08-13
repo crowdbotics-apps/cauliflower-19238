5edb128125556c97fe11bc234edfbd8e1aa71caf
Crowdbotics <team@crowdbotics.com>
2020-08-13 15:03:21 +0000

    Update components

--- src/features/BlankScreen989890/screens/blank.js
+++ src/features/BlankScreen989890/screens/blank.js
@@ -1300,6 +1300,41 @@ export class _Blank extends React.Component {
                     backgroundSize: "auto"
                   }}
                 >
+                  <Icon
+                    iconFont="Eva Design Icons"
+                    name="shopping-cart"
+                    width={20}
+                    height={20}
+                    style={{
+                      marginLeft: 0,
+                      marginRight: 0,
+                      marginTop: 5,
+                      marginBottom: 5,
+                      paddingLeft: 5,
+                      paddingRight: 5,
+                      paddingTop: 5,
+                      paddingBottom: 5,
+                      overflow: "visible",
+                      textAlign: "left",
+                      verticalAlign: "baseline",
+                      borderColor: "#000000",
+                      borderStyle: "solid",
+                      borderWidth: 0,
+                      borderLeftWidth: 0,
+                      borderRightWidth: 0,
+                      borderTopWidth: 0,
+                      borderBottomWidth: 0,
+                      borderRadius: 0,
+                      fontSize: 20,
+                      color: "#000000",
+                      backgroundColor: "#ffffff",
+                      fontFamily: "Roboto-Regular",
+                      textDecorationLine: "none",
+                      textTransform: "none",
+                      lineHeight: 12,
+                      letterSpacing: 0
+                    }}
+                  />
                   <Button
                     style={{
                       width: "100%",
